c0d0ea2c1c0c7ea787a6fc81a5b84937c08ce995
Ashleigh Barker <barkerashleigh1@gmail.com>
2020-02-16 21:30:19 -0500

    Lab3 upload

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,18 @@
 from flask import Flask
+from flask_mail import Mail
+from flask_wtf.csrf import CSRFProtect
+
+
+csrf = CSRFProtect()
+
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '95b257ab06509d95dd64848a1a323b0dd399657a75b2b6685f'
+csrf.init_app(app)
+app.config['MAIL_SERVER'] = 'smtp.mailtrap.io' 
+app.config['MAIL_PORT'] = '465' # (or try 2525) 
+app.config['MAIL_USERNAME'] = 'username'
+app.config['MAIL_PASSWORD'] = 'password'
+
+mail = Mail(app)
 from app import views
---
+++ app/forms.py
@@ -0,0 +1,10 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, TextAreaField
+from wtforms.validators import DataRequired
+
+
+class ContactForm(FlaskForm):
+    name = StringField('Name', validators=[DataRequired()])
+    email = StringField('E-mail', validators=[DataRequired()])
+    subject = StringField('Subject', validators=[DataRequired()])
+    message = TextAreaField('Message', validators=[DataRequired()])
